8987bfa4f9bf9017693c3e222ef8d55792884ec4
Allen Chien <allen71090@gmail.com>
2025-03-13 20:36:53 -0600

    fix stop_reac

--- stop_react.sh
+++ stop_react.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Ports for Flask and React
+# Ports for React
 REACT_PORT=5173
 
 echo "---------------------------------------------"
 echo "STOPPING HACKERBOT WEB APPLICATION"
 echo "---------------------------------------------"
 
-# Find and kill React process
+# Find and kill React process by port
 REACT_PID=$(lsof -ti :$REACT_PORT)
 if [ -n "$REACT_PID" ]; then
     echo "Stopping React frontend (PID: $REACT_PID)..."
     kill "$REACT_PID"
     echo "React frontend stopped."
 else
-    echo "React frontend not running."
+    echo "React frontend not running on port $REACT_PORT."
 fi
 
 echo "---------------------------------------------"
